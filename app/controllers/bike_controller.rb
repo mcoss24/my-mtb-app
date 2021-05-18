5e38048a9ac3f78f609d0aa4d548a36b89601933
mcoss24 <mcoss24@yahoo.com>
2021-05-18 21:40:36 +0000

    table stuff

--- app/controllers/bike_controller.rb
+++ app/controllers/bike_controller.rb
@@ -13,12 +13,12 @@ class BikeController < ApplicationController
     
   def selected
     #Parameters: {"winter"=>"true", "east"=>"true", "desert"=>"true", "downhill"=>"true", "destination"=>"destination"}
-    list = params.keys()
-
-    season = list.at(0)
-    location = list.at(1)
-    climate = list.at(2)
-    type = list.at(4)
+    @list = params.keys()
+    
+    season = @list.at(0)
+    location = @list.at(1)
+    climate = @list.at(2)
+    type = @list.at(4)
 
     render({:template => "bike_templates/selected.html.erb"})
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
 get("/", {:controller => "bike", :action => "index"})
 #get("/filler", {:controller => "bike", :action => ""})
+get("/selected", {:controller => "bike", :action => "selected"})
 get("/:destination", {:controller => "bike", :action => "destination"})
+
 
 
 end

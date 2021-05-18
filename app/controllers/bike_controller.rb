13639a8f7b29259a8f5621e3f12a87527dd8916b
mcoss24 <mcoss24@yahoo.com>
2021-05-18 19:42:22 +0000

    params has is still not working

--- app/controllers/bike_controller.rb
+++ app/controllers/bike_controller.rb
@@ -1,22 +1,27 @@
 class BikeController < ApplicationController
 
-  def index
-
-    
-    #Parameters: {"season"=>"true", "location"=>"true", "climate"=>"true", "type"=>"true", "destination"=>"action_page"}
-
-    
+  def index   
     render({:template => "bike_templates/index.html.erb"})
   end
 
   def destination
-    
-    @locale = params.fetch("destination")
 
     @spot = Spot.all
 
     render({:template => "bike_templates/destination.html.erb"})
   end
+    
+  def selected
+    #Parameters: {"winter"=>"true", "east"=>"true", "desert"=>"true", "downhill"=>"true", "destination"=>"destination"}
+    list = params.keys()
 
+    season = list.at(0)
+    location = list.at(1)
+    climate = list.at(2)
+    type = list.at(4)
+
+    render({:template => "bike_templates/selected.html.erb"})
+  end
+    
 end
 

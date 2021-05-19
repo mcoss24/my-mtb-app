9e14da21c7ed6f7939fbbe950d14c29fc1e29d6a
mcoss24 <mcoss24@yahoo.com>
2021-05-19 15:03:44 +0000

    params working

--- app/controllers/bike_controller.rb
+++ app/controllers/bike_controller.rb
@@ -14,12 +14,15 @@ class BikeController < ApplicationController
   def selected
     #Parameters: {"winter"=>"true", "east"=>"true", "desert"=>"true", "downhill"=>"true", "destination"=>"destination"}
     @list = params.keys()
+    #@values = params.fetch()
     
-    season = @list.at(0)
-    location = @list.at(1)
-    climate = @list.at(2)
-    type = @list.at(4)
+    @season = @list.at(0)
+    @geography = @list.at(1)
+    @climate = @list.at(2)
+    @type = @list.at(3)
 
+    @recc = Spot.where({@season => true}).where({@geography => true}).where({@climate => true}).where({@type => true}).first.location
+    
     render({:template => "bike_templates/selected.html.erb"})
   end
     
